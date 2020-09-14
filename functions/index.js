7c55e619638f1b1f2a59973989cfb079750f6aef
Rahmat Sulistio <rr7465457@gmail.com>
2020-09-14 13:57:55 +0700

    Cloud functions: Added stripe payment process API

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,32 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")(
+  "sk_test_51HQt7mDPGnFPZHLuGICoJDzNlmQigWBPqCgrrk8LxyO5641ojQOnLIpGOEm3HFHBy7VfnirPuVjRP2LzCAyDZ0Mr00gLEcfxDQ"
+);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// App Config
+const app = express();
+
+// Middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// API routes
+app.get("/", (req, res) => res.status(200).send("hello world"));
+
+app.post("/payments/create", async (req, res) => {
+  const { total } = req.query;
+
+  console.log("Recieved", total);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total, //subunits of the currency
+    currency: "usd",
+  });
+
+  res.status(201).send({ clientSecret: paymentIntent.client_secret });
+});
+
+// Listen command
+exports.api = functions.https.onRequest(app);
